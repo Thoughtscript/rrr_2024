1677c0a65f4673de65386f0515f427284e725206
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-07-07 13:02:10 -0500

    improve examples

--- ruby/3-dependencies/main.rb
+++ ruby/3-dependencies/main.rb
@@ -1,4 +1,10 @@
-require "./dependency.rb"
+# require "./dependency.rb"
+require_relative  "dependency"
+# Use require_relative here over require
+## Since script is called in root dir
+### https://stackoverflow.com/questions/9750610/ruby-require-error-cannot-load-such-file
+
+
 # Imports are made possible by an explicit require statement
 ## No import affix required
 ## Use relative filepath syntax to locate file if not using a module
--- ruby/run.sh
+++ ruby/run.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-ruby main.rb &
+ruby main.rb
 
-ruby 1-main_method/main.rb &
-ruby 2-methods/main.rb &
-ruby 3-dependencies/main.rb &
-ruby 4-loops/main.rb &
-ruby 5-declarations/main.rb &
-ruby 6-ood/main.rb &
-ruby 7-arrays/main.rb &
-ruby 8-closures/main.rb &
-ruby 9-strings/main.rb &
-ruby 10-hashes/main.rb &
-ruby 11-modules/main.rb &
-ruby 12-access/main.rb &
-ruby 13-control_flow/main.rb &
-ruby 14-it_primitives/main.rb &
-ruby 15-numbers/main.rb &
-ruby 16-safe_navigation/main.rb &
-ruby 17-sym_str/main.rb &
+echo "================ 1-main_method ================" && ruby 1-main_method/main.rb
+echo "================ 2-methods ================" && ruby 2-methods/main.rb
+echo "================ 3-dependencies ================" && ruby 3-dependencies/main.rb
+echo "================ 4-loops ================" && ruby 4-loops/main.rb
+echo "================ 5-declarations ================" && ruby 5-declarations/main.rb
+echo "================ 6-ood ================" && ruby 6-ood/main.rb
+echo "================ 7-arrays ================" && ruby 7-arrays/main.rb
+echo "================ 8-closures ================" && ruby 8-closures/main.rb
+echo "================ 9-strings ================" && ruby 9-strings/main.rb
+echo "================ 10-hashes ================" && ruby 10-hashes/main.rb
+echo "================ 11-modules ================" && ruby 11-modules/main.rb
+echo "================ 12-access ================" && ruby 12-access/main.rb
+echo "================ 13-control_flow ================" && ruby 13-control_flow/main.rb
+echo "================ 14-it_primitives ================" && ruby 14-it_primitives/main.rb
+echo "================ 15-numbers ================" && ruby 15-numbers/main.rb
+echo "================ 16-safe_navigation ================" && ruby 16-safe_navigation/main.rb
+echo "================ 17-sym_str ================" && ruby 17-sym_str/main.rb
 
 wait
